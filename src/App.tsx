9841de5319e0c3abf3c5f56c1a5184005f86995c
Berhan Zencir <berzencir001@gmail.com>
2023-12-16 20:33:44 +0300

    Added url full screen feature.

--- src/App.tsx
+++ src/App.tsx
@@ -57,6 +57,7 @@ const App = () => {
           originWhitelist={['*']}
           allowsInlineMediaPlayback
           javaScriptEnabled
+          allowsFullscreenVideo={true}
           scalesPageToFit
           mediaPlaybackRequiresUserAction={false}
           javaScriptEnabledAndroid
